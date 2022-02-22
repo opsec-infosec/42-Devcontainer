9639fb447d60d62673c74ea1aa04d9ccd3ae95d0
Dale Furneaux <74446520+opsec-infosec@users.noreply.github.com>
2022-02-23 00:27:21 +0400

    Update helloworld.c
    Fixed Formatting... updated header

--- src/helloworld.c
+++ src/helloworld.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   helloworld.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dfurneau <dfurneau@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dfurneau <dfurneau@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/05 10:38:03 by dfurneau          #+#    #+#             */
 /*   Updated: 2021/10/05 10:38:07 by dfurneau         ###   ########.fr       */
@@ -12,9 +12,8 @@
 
 #include <stdio.h>
 
-int	main(void)
-{
-	printf(("Welcome to 42\n"));
-	printf("Devcontainer with norminette from opinfosec\n");
-	return (0);
+int	main(void) {
+    printf(("Welcome to 42\n"));
+    printf("Devcontainer with norminette from opinfosec\n");
+    return (0);
 }
